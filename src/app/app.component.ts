2232851be7bd5fe7a6bf5e613fbe330a2eed2dec
Hunter <hjkalbert@rocketmail.com>
2021-09-23 13:16:23 -0400

    Getting ready for auth

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,9 @@
 import { Component } from '@angular/core';
+import {Firestore, collectionData, collection} from '@angular/fire/firestore'
 
+interface Item {
+  name: string,
+}
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
